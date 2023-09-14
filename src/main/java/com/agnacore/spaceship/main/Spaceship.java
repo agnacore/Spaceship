a17c87f0f87edd654f36bbdbc37c66c6b5b52526
Christopher M Green <cgvoice35@gmail.com>
2023-09-13 23:04:29 -0500

    Add JavaFX "hello world"

--- src/main/java/com/agnacore/spaceship/main/Spaceship.java
+++ src/main/java/com/agnacore/spaceship/main/Spaceship.java
@@ -1,10 +1,23 @@
 package com.agnacore.spaceship.main;
 
-public class Spaceship {
+import javafx.application.Application;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
+
+public class Spaceship extends Application {
+
+	public static void main(String[] args)
+	{
+		launch();
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
 		// start the game
-		System.out.println("Hello, world!");
+		Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+		alert.setContentText("Hello, world!");
+		alert.showAndWait();
 	}
 
 }
