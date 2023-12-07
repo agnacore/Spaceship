bcbb64c2c5af824539b50b5dd08bc860082ea404
Christopher M Green <cgvoice35@gmail.com>
2023-12-06 21:09:13 -0600

    [TEMP] Add pop-up

--- src/main/java/com/agnacore/spaceship/application/MainMenuController.java
+++ src/main/java/com/agnacore/spaceship/application/MainMenuController.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -24,7 +25,14 @@ public class MainMenuController {
     }
 
     @FXML
-    public void handleScoresButtonClicked(ActionEvent event) { }
+    public void handleScoresButtonClicked(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setContentText("And this is where I'd display the list of high scores...");
+        alert.showAndWait();
+        Alert alert1 = new Alert(Alert.AlertType.WARNING);
+        alert1.setContentText("IF I HAD ONE!");
+        alert1.showAndWait();
+    }
 
     @FXML
     public void handleExitButtonClicked(ActionEvent event) {
